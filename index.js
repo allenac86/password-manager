2dbbd1d4baa5901882bfdf213e1464c2b441798b
Adam Allen <allenac86@gmail.com>
2022-02-03 12:09:21 -0500

    add env PORT variable

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const connectToDB = require('./database/database');
-const accountRoutes = require('./routes/account-routes');
-require('dotenv').config();
+const express = require("express");
+const connectToDB = require("./database/database");
+const accountRoutes = require("./routes/account-routes");
+require("dotenv").config();
 
-const PORT = process.env.port || 4200;
+const PORT = process.env.PORT || 4200;
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-	res.redirect('/api/v1/passwords');
+app.get("/", (req, res) => {
+  res.redirect("/api/v1/passwords");
 });
 
-app.use('/api/v1/passwords', accountRoutes);
+app.use("/api/v1/passwords", accountRoutes);
 
 const start = async () => {
-	try {
-		await connectToDB(process.env.MONGO_URI);
-		app.listen(PORT, console.log(`Server listening on port ${PORT}`));
-	} catch (err) {
-		console.error(err);
-	}
+  try {
+    await connectToDB(process.env.MONGO_URI);
+    app.listen(PORT, console.log(`Server listening on port ${PORT}`));
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 start();
