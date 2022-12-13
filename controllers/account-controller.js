7dc8528f7bb47c56c6268fd0852d2384b82788f2
Adam Allen <allenac86@gmail.com>
2022-12-12 22:13:17 -0500

    add fix for token not present error

--- controllers/account-controller.js
+++ controllers/account-controller.js
@@ -5,40 +5,45 @@ const { createCustomError } = require('../errors/CustomError');
 
 // GET all accounts
 const getAllAccounts = asyncHandler(async (req, res) => {
-	const token = req.headers['authorization'].split(' ')[1];
+	try {
+		const token = req.headers['authorization'].split(' ')[1];
 
-	if (!token) res.status(400).send('token not present');
+		jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+			if (err) res.status(403).send('invalid token');
+		});
 
-	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-		if (err) res.status(403).send('invalid token');
-	});
-
-	const accounts = await Account.find({});
-	res.status(200).json({ numAccounts: accounts.length, accounts });
+		const accounts = await Account.find({});
+		res.status(200).json({ numAccounts: accounts.length, accounts });
+	} catch (err) {
+		res.status(400).send('token not present');
+	}
 });
 
 // POST new account
 const createAccount = asyncHandler(async (req, res) => {
-	const token = req.headers['authorization'].split(' ')[1];
 	const { url, username, password } = req.body;
 
-	if (!token) res.status(400).send('token not present');
+	try {
+		const token = req.headers['authorization'].split(' ')[1];
 
-	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-		if (err) res.status(403).send('invalid token');
-	});
+		jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+			if (err) res.status(403).send('invalid token');
+		});
 
-	if (password.length > 20 || password.length < 12) {
-		res.status(400).send('password must be between 12 and 20 characters');
+		if (password.length > 20 || password.length < 12) {
+			res.status(400).send('password must be between 12 and 20 characters');
+		}
+
+		const account = await Account.create({
+			url,
+			username,
+			password,
+		});
+
+		res.status(201).json({ msg: 'Account created', account });
+	} catch (err) {
+		res.status(400).send('token not present');
 	}
-
-	const account = await Account.create({
-		url,
-		username,
-		password,
-	});
-
-	res.status(201).json({ msg: 'Account created', account });
 });
 
 // GET single account by id
