4f67dc8d141f54b705ff55ab40a77e8d6749dca0
Adam Allen <allenac86@gmail.com>
2022-12-15 23:05:44 -0500

    added encrypt/decrypt functions and tests

---
+++ crypto.test.js
@@ -0,0 +1,27 @@
+const { encrypt, decrypt } = require('./crypto');
+
+const testObj = {
+	msg: 'Hello World!',
+	date: '12/14/2022',
+	name: 'shady',
+};
+
+const hash = encrypt(
+	JSON.stringify({
+		msg: 'Hello World!',
+		date: '12/14/2022',
+		name: 'shady',
+	})
+);
+
+const stringifiedTestObj = JSON.stringify(testObj);
+
+const decryptedString = decrypt(hash);
+
+test('the encrypted test object does not match the stringified test object', () => {
+	expect(hash).toEqual(expect.not.stringMatching(/JSON.stringify(testObj)/));
+});
+
+test('the decrypted hash string matches the stringified test object', () => {
+	expect(stringifiedTestObj).toBe(decryptedString);
+});
