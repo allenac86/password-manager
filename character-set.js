551a5716be60dc766a2e6d9c5d8462814edf379e
Adam Allen <allenac86@gmail.com>
2023-04-29 23:24:38 -0400

    change encryption algorithm and secret key and stored them in env, remove unused code

--- character-set.js
+++
@@ -1,8 +0,0 @@
-const characterSet = {
-  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  lowercase: "abcdefghijklmnopqrstuvwxyz",
-  numbers: "1234567890",
-  special: "!#$*.-_",
-};
-
-module.exports = characterSet;
