c7b0d39c6ad9e5fae6ae2b2e1bf52243a45bfe92
Adam Allen <allenac86@gmail.com>
2023-06-23 10:29:33 -0400

    add unit tests

--- crypto.test.js
+++ crypto.test.js
@@ -3,14 +3,14 @@ const { encrypt, decrypt } = require('./crypto');
 const testObj = {
 	msg: 'Hello World!',
 	date: '12/14/2022',
-	name: 'skele',
+	name: 'test',
 };
 
 const hash = encrypt(
 	JSON.stringify({
 		msg: 'Hello World!',
 		date: '12/14/2022',
-		name: 'skele',
+		name: 'test',
 	})
 );
 
@@ -18,10 +18,12 @@ const stringifiedTestObj = JSON.stringify(testObj);
 
 const decryptedString = decrypt(hash);
 
-test('the encrypted test object does not match the stringified test object', () => {
-	expect(hash).toEqual(expect.not.stringMatching(/JSON.stringify(testObj)/));
-});
-
-test('the decrypted hash string matches the stringified test object', () => {
-	expect(stringifiedTestObj).toBe(decryptedString);
+describe('crypto tests', () => {
+	test('the encrypted test object does not match the stringified test object', () => {
+		expect(hash).toEqual(expect.not.stringMatching(/JSON.stringify(testObj)/));
+	});
+	
+	test('the decrypted hash string matches the stringified test object', () => {
+		expect(stringifiedTestObj).toBe(decryptedString);
+	});
 });
